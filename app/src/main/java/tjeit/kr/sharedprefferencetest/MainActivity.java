6074bae1a895cc0e700f40accde8524887d38f5d
Hottory <gkffndnl123@naver.com>
2018-12-29 15:11:25 +0900

    이메일 저장/ 불러오기 액티비티 연동

--- app/src/main/java/tjeit/kr/sharedprefferencetest/MainActivity.java
+++ app/src/main/java/tjeit/kr/sharedprefferencetest/MainActivity.java
@@ -1,14 +1,23 @@
 package tjeit.kr.sharedprefferencetest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import tjeit.kr.sharedprefferencetest.utils.ContextUtil;
 
 public class MainActivity extends BaseActivity {
+
+    private android.widget.EditText emailEdt;
+    private android.widget.Button emailSaveBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         bindViews();
         setupEvents();
         setValues();
@@ -17,15 +26,32 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        emailSaveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ContextUtil.setEmail(mContext, emailEdt.getText().toString());
+
+                Toast.makeText(mContext, "입력한 이메일이 저장되었습니다", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
     @Override
     public void setValues() {
 
+
+        emailEdt.setText(ContextUtil.getEmail(mContext));
+
     }
 
     @Override
     public void bindViews() {
+
+        this.emailSaveBtn = (Button) findViewById(R.id.emailSaveBtn);
+        this.emailEdt = (EditText) findViewById(R.id.emailEdt);
 
     }
 }
